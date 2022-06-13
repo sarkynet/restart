144a0c62c4821b4ebb5087176bf6b35635664fb4
Jude Ejeziem <sarkynet4wisdom@yahoo.com>
2022-06-13 17:55:06 +0100

    Bootstrap, Material and UI Notification Set Up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'restart';
+
+  constructor(private toastr: ToastrService) {}
+
+  showSuccess() {
+    this.toastr.success('Hello world!!!', 'SUCCESSFUL');
+  }
+  showError() {
+    this.toastr.error('Hello world!!!', 'ERROR');
+  }
+  showWarning() {
+    this.toastr.warning('Hello world!!!', 'WARNING');
+  }
+  showInfo() {
+    this.toastr.info('Hello world!!!', 'INFO');
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
+// import { SnackbarProvider } from 'notistack';
+import { MaterialModule } from '../app/material/material.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +14,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'serverApp' }),
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    ToastrModule.forRoot({
+      positionClass: 'toast-top-center',
+    })
+
   ],
   providers: [],
   bootstrap: [AppComponent]
